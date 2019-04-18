a48a2f2e55e1007c9eebf48abc4881d576aa856c
Loukas Litsos <lkslts64@gmail.com>
2019-04-18 21:56:30 +0300

    My visitors are now located in a package + updated makefile. Parser now can take multiple files as input.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import myvisitors.SymbolTableVisitor;
+import myvisitors.TypeCheckerVisitor;
 import syntaxtree.*;
 import visitor.*;
 import java.io.*;
@@ -5,41 +7,38 @@ import symboltable.*;
 
 
 class Main {
-    public static void main (String [] args){
-	if(args.length != 1){
-	    System.err.println("Usage: java Driver <inputFile>");
-	    System.exit(1);
-	}
-	FileInputStream fis = null;
-	try{
-	    fis = new FileInputStream(args[0]);
-	    MiniJavaParser parser = new MiniJavaParser(fis);
-	    System.err.println("Program parsed successfully.");
-	    SymbolTableVisitor sym = new SymbolTableVisitor();
-	    Goal root = parser.Goal();
-	    //System.out.println(root.accept(sym));
-		if ( root.accept(sym,null) == "ERROR" )
-			System.exit(0);
-		System.out.println("---------------------------------------------");
-		//sym.symbolTable.display_contents();
-		//sym.symbolTable.print_offsets();
-		TypeCheckerVisitor typeCheckerVisitor = new TypeCheckerVisitor(sym.symbolTable);
-		root.accept(typeCheckerVisitor,null);
-	}
-	catch(ParseException ex){
-	    System.out.println(ex.getMessage());
-	}
-	catch(FileNotFoundException ex){
-	    System.err.println(ex.getMessage());
-	}
-	finally{
-	
-	    try{
-		if(fis != null) fis.close();
-	    }
-	    catch(IOException ex){
-		System.err.println(ex.getMessage());
-	    }
-	}
-	}
+    public static void main (String [] args) {
+        if (args.length < 1) {
+            System.err.println("Usage: java Driver <inputFile>");
+            System.exit(1);
+        }
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("---------------------------------------------");
+            FileInputStream fis = null;
+            try {
+                fis = new FileInputStream(args[i]);
+                MiniJavaParser parser = new MiniJavaParser(fis);
+                SymbolTableVisitor sym = new SymbolTableVisitor();
+                Goal root = parser.Goal();
+                //System.out.println(root.accept(sym));
+                if (root.accept(sym, null) == "ERROR")
+                    System.exit(0);
+                //sym.symbolTable.display_contents();
+                //sym.symbolTable.print_offsets();
+                TypeCheckerVisitor typeCheckerVisitor = new TypeCheckerVisitor(sym.symbolTable);
+                root.accept(typeCheckerVisitor, null);
+            } catch (ParseException ex) {
+                System.out.println(ex.getMessage());
+            } catch (FileNotFoundException ex) {
+                System.err.println(ex.getMessage());
+            } finally {
+
+                try {
+                    if (fis != null) fis.close();
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+                }
+            }
+        }
+    }
 }
